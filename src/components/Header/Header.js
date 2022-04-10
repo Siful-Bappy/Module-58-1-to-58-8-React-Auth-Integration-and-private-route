98b3c7d0c18ec00907d453104cf7041951e49996
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-10 16:13:30 +0900

    till 2nd video done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,10 @@ const Header = () => {
     <div className="header">
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/prducts">Products</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/about up">About us</Link>
         <Link to="/login">Login</Link>
       </nav>
     </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login section</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is login section</h2>
+      <div>
+        <button style={{margin: "20px", padding: "10px 20px", borderRadius: "10px", border: "doted"}}>Google Sign In</button>
+      </div>
+      <form action="">
+        <input type="email" name="" id="" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="Your Password" />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
